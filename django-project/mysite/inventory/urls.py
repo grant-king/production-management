d3fa46fee9325c15a9386466f724d460add84068
Grant King <grantryan.king@gmail.com>
2021-02-16 21:51:17 -0800

    Add product_detail and CustomerCustomerOrderList views
    Add views, routes, templates for rendering two new page types:
    
    1. Product Detail Page - page to display information related to an individual product, like how much is available as calculated by summing back stock, customer orders output and purchase orders input.
    
    2. Customer Orders per Customer Page - page to display all customer orders related to an individual customer.

--- django-project/mysite/inventory/urls.py
+++ django-project/mysite/inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from inventory.views import ProductCustomerOrderList, ProductPurchaseOrderList
+from inventory.views import ProductCustomerOrderList, ProductPurchaseOrderList, product_detail, CustomerCustomerOrderList
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('customer_orders/<product>/', ProductCustomerOrderList.as_view(), name='product_customer_orders'),
     path('purchase_orders/<product>/', ProductPurchaseOrderList.as_view(), name='product_purchase_orders'),
+    path('customers/<customer>/', CustomerCustomerOrderList.as_view(), name='customer_customer_orders'),
+    path('products/<product>/', views.product_detail, name='product_detail'),
 ]
--- django-project/mysite/inventory/views.py
+++ django-project/mysite/inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
-from .models import CustomerOrder, PurchaseOrder, Product
+from .models import CustomerOrder, PurchaseOrder, Product, Customer
 
 def index(request):
     product_list = Product.objects.all()
@@ -12,7 +12,7 @@ def index(request):
         rco_sum = sum([customer_order.quantity for customer_order in related_customer_orders])
         related_purchase_orders = PurchaseOrder.objects.filter(product=item)
         rpo_sum = sum([purchase_order.total for purchase_order in related_purchase_orders])
-        calculated_inventories.append(item.back_stock + rpo_sum - rco_sum)
+        calculated_inventories.append(item.inventory + rpo_sum - rco_sum)
         rpo_sums.append(rpo_sum)
         rco_sums.append(rco_sum)
 
@@ -20,13 +20,29 @@ def index(request):
 
     return render(request, 'inventory/index.html', context)
 
+def product_detail(request, product):
+    product = Product.objects.get(label=product)
+    related_customer_orders = CustomerOrder.objects.filter(product=product)
+    rco_sum = sum([customer_order.quantity for customer_order in related_customer_orders])
+    related_purchase_orders = PurchaseOrder.objects.filter(product=product)
+    rpo_sum = sum([purchase_order.total for purchase_order in related_purchase_orders])
+    calculated_inventory = product.inventory + rpo_sum - rco_sum
+    context = {
+        'product': product,
+        'available': calculated_inventory,
+        'customer_orders': rco_sum,
+        'purchase_orders': rpo_sum,
+    }
+    
+    return render(request, 'inventory/product_detail.html', context)
+
 
 class ProductCustomerOrderList(ListView):
     template_name = 'inventory/product_customer_orders.html'
     
     def get_queryset(self):
         self.product = get_object_or_404(Product, label=self.kwargs['product'])
-        return CustomerOrder.objects.filter(product=self.product)
+        return CustomerOrder.objects.filter(product=self.product).order_by('date')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -40,10 +56,24 @@ class ProductPurchaseOrderList(ListView):
     
     def get_queryset(self):
         self.product = get_object_or_404(Product, label=self.kwargs['product'])
-        return PurchaseOrder.objects.filter(product=self.product)
+        return PurchaseOrder.objects.filter(product=self.product).order_by('date')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['product'] = self.product
+
+        return context
+
+
+class CustomerCustomerOrderList(ListView):
+    template_name = 'inventory/customer_customer_orders.html'
+    
+    def get_queryset(self):
+        self.customer = get_object_or_404(Customer, label=self.kwargs['customer'])
+        return CustomerOrder.objects.filter(customer=self.customer).order_by('date')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['customer'] = self.customer
 
         return context
