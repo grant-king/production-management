59f2dcc0cbf661ea47914f78f06d0f5b95f2c001
Grant King <grantryan.king@gmail.com>
2021-04-04 00:53:50 -0700

    Add User Profile image functionality
    Add settings, directories, and static route to enable media file serving.  Add company logo image to user profile template. Update Profile model.
    
    Add media directory contents to gitignore.

--- django-project/mysite/users/models.py
+++ django-project/mysite/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company_logo = models.ImageField(default='default.jpg', upload_to='company_logos')
+    company_logo = models.ImageField(default='default.png', upload_to='company_logos')
 
     def __str__(self):
         return f'{self.user.username} Profile'
