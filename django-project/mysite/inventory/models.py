585db98252fe50a5c86e39c96d30d74bef7248df
Grant King <grantryan.king@gmail.com>
2021-04-05 01:06:02 -0700

    Update Product model
    Add user attribute to Product model as foreign key relation to User.  This allows for each user to be able to access their own set of Products and other related objects.
    
    Add migrations. Update database.

--- django-project/mysite/inventory/models.py
+++ django-project/mysite/inventory/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
     label = models.SlugField(max_length=40, null=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f'{self.name}'
