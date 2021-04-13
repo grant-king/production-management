93128c63db11d38a70bf13e9507ac89835ad8f7d
Grant King <grantryan.king@gmail.com>
2021-04-12 22:38:44 -0700

    Add User password reset interface
    Add password reset interface for User accounts. Add settings, routes with generic auth views, templates, and links to enable this functionality.

--- django-project/mysite/users/urls.py
+++ django-project/mysite/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', success_url=reverse_lazy('users:password_reset_done'), email_template_name='users/password_reset_email.html'), name='password_reset'),
+    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
