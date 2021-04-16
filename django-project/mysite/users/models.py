a6c9873820f35f81a1c98890c3c725d0dafc4562
Grant King <grantryan.king@gmail.com>
2021-04-16 05:36:16 -0700

    Fix image update error
    Fix error caused by new storages configuration that prevented User from being able to update their Profile.company_logo image.

--- django-project/mysite/users/models.py
+++ django-project/mysite/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.files.storage import default_storage as storage
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,11 +14,14 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.company_logo.path)
+        img = Image.open(self.company_logo)
+
         MAX_WIDTH = 300 
 
         if img.width > MAX_WIDTH:
             resize_ratio = MAX_WIDTH / img.width
             output_size = (int(img.width*resize_ratio), int(img.height*resize_ratio))
             img = img.resize(output_size)
-            img.save(self.company_logo.path)
+            file_handler = storage.open(self.company_logo.name, "w")
+            img.save(file_handler, 'png')
+            file_handler.close()            
