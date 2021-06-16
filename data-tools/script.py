97ded42b84bf9f52cec17ff14f5f53b714b1e190
Grant King <grantryan.king@gmail.com>
2021-06-15 18:22:34 -0700

    Update to use new tool models
    Add calls to utilize new data_tool classes.
    
    Script imports data from two external source csvs that contain particular column headers. Add calls that use new class methods to relate, filter by defined date ranges, aggregate, and graph the results.

--- data-tools/script.py
+++ data-tools/script.py
@@ -1,10 +1,35 @@
-from tool_models.data_tools import LocationData
+from tool_models.data_tools import LocationData, LocationSales, SalesData
 
 def location_report():
     location_data = LocationData('locations.csv')
-    location_data.print_totals_reports()
-    print(location_data.get_region_data())
-    location_data.show_totals_reports_graphs()
+    sales_data = SalesData('sales.csv')
+    location_sales = LocationSales(location_data.location_relations, sales_data.sales_df)
+    quarters = {
+        '1q2021': {'start_date': '01/01/2021', 'end_date': '03/31/2021'}, 
+        '2q2021': {'start_date': '04/01/2021', 'end_date': '06/30/2021'},
+        }
+    region_comparison_dict = {}
+    metro_comparison_dict = {}
+    for quarter, dates in quarters.items():
+        date_range = [dates['start_date'], dates['end_date']]
+        
+        #get data and graph of aggregated region product sales
+        region_sales = location_sales.get_date_filter_aggregated_sales(
+            date_range, group_by=['Region', 'Product'])
+        location_sales.date_filter_grouped_product_sales_plot(region_sales)
+        region_sales.to_csv(f'{quarter}region_sales.csv')
+        region_comparison_dict['quarter'] = region_sales
+        
+        #get data and graph of aggregated market-metro and product sales
+        metro_sales = location_sales.get_date_filter_aggregated_sales(
+            date_range, group_by=['Market-Metro', 'Product'])
+        location_sales.date_filter_grouped_product_sales_plot(metro_sales)
+        metro_sales.to_csv(f'{quarter}metro_sales.csv')
+        metro_comparison_dict['quarter'] = metro_sales
+
+
+
+
 
 if __name__ == '__main__':
     print('running location report')
